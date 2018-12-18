4ea6368125484f6daa2eaa057fb4a2123e372413
David Pirraglia <DPirraglia1@pride.hofstra.edu>
2018-12-17 19:57:22 -0500

    The game will now reset itself upon winning or losing

--- DavidPirragliaMemoryGame/ViewController.swift
+++ DavidPirragliaMemoryGame/ViewController.swift
@@ -26,8 +26,28 @@ class ViewController: UIViewController {
     }
 
     @IBOutlet weak var lblMovesLeft: UILabel!
-
     @IBOutlet weak var lblMovesMade: UILabel!
+
+    @IBOutlet weak var button0: UIButton!
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var button3: UIButton!
+    @IBOutlet weak var button4: UIButton!
+    @IBOutlet weak var button5: UIButton!
+    @IBOutlet weak var button6: UIButton!
+    @IBOutlet weak var button7: UIButton!
+    @IBOutlet weak var button8: UIButton!
+    @IBOutlet weak var button9: UIButton!
+    @IBOutlet weak var button10: UIButton!
+    @IBOutlet weak var button11: UIButton!
+    @IBOutlet weak var button12: UIButton!
+    @IBOutlet weak var button13: UIButton!
+    @IBOutlet weak var button14: UIButton!
+    @IBOutlet weak var button15: UIButton!
+    @IBOutlet weak var button16: UIButton!
+    @IBOutlet weak var button17: UIButton!
+    @IBOutlet weak var button18: UIButton!
+    @IBOutlet weak var button19: UIButton!
 
     @IBAction func btnClick(_ sender: UIButton) {
         var label: String = ""
@@ -60,7 +80,9 @@ class ViewController: UIViewController {
                         message: "Congratulations",
                         preferredStyle: UIAlertControllerStyle.alert)
                     alert.addAction(UIAlertAction(title: "OK",
-                        style: UIAlertActionStyle.default, handler: nil))
+                        style: UIAlertActionStyle.default, handler: {action in
+                            self.reset()
+                    }))
                     self.present(alert, animated: true, completion: nil)
                 }
             } else {
@@ -80,7 +102,9 @@ class ViewController: UIViewController {
                         message: "Better luck next time",
                         preferredStyle: UIAlertControllerStyle.alert)
                     alert.addAction(UIAlertAction(title: "OK",
-                        style: UIAlertActionStyle.default, handler: nil))
+                        style: UIAlertActionStyle.default, handler: {action in
+                            self.reset()
+                    }))
                     self.present(alert, animated: true, completion: nil)
                 }
             }
@@ -101,6 +125,28 @@ class ViewController: UIViewController {
         button.titleLabel!.text = ""
         button.setBackgroundImage((UIImage(named: "card.jpg") as UIImage?)!,
             for: UIControlState.normal)
+    }
+
+    func reset() {
+        game.reset()
+
+        lblMovesMade.text = "0"
+        lblMovesLeft.text = "50"
+
+        lastButtons = []
+        lastTag = -1
+
+        let allButtons: [UIButton] = [
+            button0,  button1,  button2,  button3,
+            button4,  button5,  button6,  button7,
+            button8,  button9,  button10, button11,
+            button12, button13, button14, button15,
+            button16, button17, button18, button19
+        ]
+
+        for button in allButtons {
+            hideCard(button)
+        }
     }
 }
 
